3fd01739a2d4d3c44ae9e4e77bcd7179a48f3e9f
Ivo van Poorten <ivopvp@gmail.com>
2023-11-16 20:24:14 +0100

    tester for stdin with TestU01

---
+++ random/test/testu01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "TestU01.h"
+
+static unsigned int from_stdin(void) {
+    int a = getc(stdin);
+    a |= getc(stdin) << 8;
+    a |= getc(stdin) << 16;
+    a |= getc(stdin) << 24;
+    return a;
+}
+
+int main (int argc, char **argv) {
+    unif01_Gen* gen = unif01_CreateExternGenBits("stdin", from_stdin);
+
+//    bbattery_SmallCrush(gen);
+    bbattery_Crush(gen);
+//    bbattery_BigCrush(gen);
+
+    return 0;
+}
+
