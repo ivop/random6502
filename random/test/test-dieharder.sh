23a76a66f9081694cdd39b901d50b827f0be49ae
Ivo van Poorten <ivopvp@gmail.com>
2023-11-16 04:02:26 +0100

    rename output files

--- random/test/test-dieharder.sh
+++ random/test/test-dieharder.sh
@@ -4,7 +4,7 @@ set -e
 
 for i in 0 1 2 3 4 5 6; do
     echo "testing algorithm $i"
-    ./rnddata $i 9223372036854775807 | dieharder -f /dev/stdin -g 201 -a > raw$i.report &
+    ./rnddata $i 9223372036854775807 | dieharder -f /dev/stdin -g 201 -a > raw$i.dh &
 done
 
 wait `jobs -p`
