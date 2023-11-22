7dd45cda31fbaa72e434651664ca81ea2ee6386e
Ivo van Poorten <ivopvp@gmail.com>
2023-11-22 05:23:34 +0100

    enable SHA-512 mode. I guess implementing this on the 6502 is ridiculous ;)

--- test/rnddata.c
+++ test/rnddata.c
@@ -310,7 +310,7 @@ static void random_sfc32_seed(struct random_sfc32_ctx *ctx, uint32_t *seed) {
 
 /* ---------------------------------------------------------------------- */
 
-// based on the SHA-256 algorithm
+// based on the SHA-2 algorithm
 // https://en.wikipedia.org/wiki/SHA-2
 // after each chunk, the hash is returned as random number
 // when we run out of bytes, the current chunk is xor-ed with the hash
@@ -321,10 +321,14 @@ static void random_sfc32_seed(struct random_sfc32_ctx *ctx, uint32_t *seed) {
 // you need to take care of the endianess of your input buffer and that of
 // ctx->chunk.
 
-#if 1
+#if 0
+
+// SHA-256
 
 #define SHA_WORD uint32_t
 #define RORW(v, r) ( ((v) << (32-(r))) | ((v) >> (r)) )
+
+#define KWSIZE 64
 
 static SHA_WORD initial_hash[8] = {
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
@@ -342,7 +346,39 @@ static SHA_WORD k[64] = {
    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 };
 
-#else   /* SHA-512 */
+#else
+
+// SHA-512
+
+#define SHA_WORD uint64_t
+#define RORW(v, r) ( ((v) << (32-(r))) | ((v) >> (r)) )
+
+#define KWSIZE 80
+
+static SHA_WORD initial_hash[8] = {
+0x6a09e667f3bcc908ull, 0xbb67ae8584caa73bull, 0x3c6ef372fe94f82bull, 0xa54ff53a5f1d36f1ull,
+0x510e527fade682d1ull, 0x9b05688c2b3e6c1full, 0x1f83d9abfb41bd6bull, 0x5be0cd19137e2179ull
+};
+
+static SHA_WORD k[KWSIZE] = {
+    0x428a2f98d728ae22ull, 0x7137449123ef65cdull, 0xb5c0fbcfec4d3b2full, 0xe9b5dba58189dbbcull, 0x3956c25bf348b538ull,
+    0x59f111f1b605d019ull, 0x923f82a4af194f9bull, 0xab1c5ed5da6d8118ull, 0xd807aa98a3030242ull, 0x12835b0145706fbeull,
+    0x243185be4ee4b28cull, 0x550c7dc3d5ffb4e2ull, 0x72be5d74f27b896full, 0x80deb1fe3b1696b1ull, 0x9bdc06a725c71235ull,
+    0xc19bf174cf692694ull, 0xe49b69c19ef14ad2ull, 0xefbe4786384f25e3ull, 0x0fc19dc68b8cd5b5ull, 0x240ca1cc77ac9c65ull,
+    0x2de92c6f592b0275ull, 0x4a7484aa6ea6e483ull, 0x5cb0a9dcbd41fbd4ull, 0x76f988da831153b5ull, 0x983e5152ee66dfabull,
+    0xa831c66d2db43210ull, 0xb00327c898fb213full, 0xbf597fc7beef0ee4ull, 0xc6e00bf33da88fc2ull, 0xd5a79147930aa725ull,
+    0x06ca6351e003826full, 0x142929670a0e6e70ull, 0x27b70a8546d22ffcull, 0x2e1b21385c26c926ull, 0x4d2c6dfc5ac42aedull,
+    0x53380d139d95b3dfull, 0x650a73548baf63deull, 0x766a0abb3c77b2a8ull, 0x81c2c92e47edaee6ull, 0x92722c851482353bull,
+    0xa2bfe8a14cf10364ull, 0xa81a664bbc423001ull, 0xc24b8b70d0f89791ull, 0xc76c51a30654be30ull, 0xd192e819d6ef5218ull,
+    0xd69906245565a910ull, 0xf40e35855771202aull, 0x106aa07032bbd1b8ull, 0x19a4c116b8d2d0c8ull, 0x1e376c085141ab53ull,
+    0x2748774cdf8eeb99ull, 0x34b0bcb5e19b48a8ull, 0x391c0cb3c5c95a63ull, 0x4ed8aa4ae3418acbull, 0x5b9cca4f7763e373ull,
+    0x682e6ff3d6b2b8a3ull, 0x748f82ee5defb2fcull, 0x78a5636f43172f60ull, 0x84c87814a1f0ab72ull, 0x8cc702081a6439ecull,
+    0x90befffa23631e28ull, 0xa4506cebde82bde9ull, 0xbef9a3f7b2c67915ull, 0xc67178f2e372532bull, 0xca273eceea26619cull,
+    0xd186b8c721c0c207ull, 0xeada7dd6cde0eb1eull, 0xf57d4f7fee6ed178ull, 0x06f067aa72176fbaull, 0x0a637dc5a2c898a6ull,
+    0x113f9804bef90daeull, 0x1b710b35131c471bull, 0x28db77f523047d84ull, 0x32caab7b40c72493ull, 0x3c9ebe0a15c9bebcull,
+    0x431d67c49c100d4cull, 0x4cc5d4becb3e42b6ull, 0x597f299cfc657e2aull, 0x5fcb6fab3ad6faecull, 0x6c44198c4a475817ull
+};
+
 #endif
 
 struct random_sha2_ctx {
@@ -352,12 +388,12 @@ struct random_sha2_ctx {
     int pos;
 };
 
-static SHA_WORD w[64];
+static SHA_WORD w[KWSIZE];
 
 static void random_sha2_handle_chunk(struct random_sha2_ctx *ctx) {
     memcpy(w, ctx->chunk, 16*sizeof(SHA_WORD));
 
-    for (int i=16; i<64; i++) {
+    for (int i=16; i<KWSIZE; i++) {
         SHA_WORD s0 = RORW(w[i-15], 7) ^ RORW(w[i-15],18) ^ RORW(w[i-15], 3);
         SHA_WORD s1 = RORW(w[i- 2],17) ^ RORW(w[i- 2],19) ^ RORW(w[i- 2],10);
         w[i] = w[i-16] + s0 + w[i-7] + s1;
@@ -372,7 +408,7 @@ static void random_sha2_handle_chunk(struct random_sha2_ctx *ctx) {
     SHA_WORD g = ctx->h[6];
     SHA_WORD h = ctx->h[7];
 
-    for (int i=0; i<64; i++) {
+    for (int i=0; i<KWSIZE; i++) {
         SHA_WORD S1 = RORW(e,6) ^ RORW(e,11) ^ RORW(e,25);
         SHA_WORD ch = (e & f) ^ ((~e) & g);
         SHA_WORD temp1 = h + S1 + ch + k[i] + w[i];
